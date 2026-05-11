44697735a90a4123d4280213ec5dca05db5041c9
williamchuang <@Zj199126h>
2026-05-11 18:19:07 +0800

    fix: uninstall tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 
-// Tailwind CSS
-import '@/assets/css/tailwind.css';
-
 // Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
