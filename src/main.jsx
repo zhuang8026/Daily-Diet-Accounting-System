fe4d579d536d30d1e4904a02921f065b5f51d72f
williamchuang <@Zj199126h>
2026-05-11 18:13:01 +0800

    feat: add tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '@/App'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import '@/assets/scss/_root.scss'
-import '@/assets/scss/index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '@/App';
+
+// Tailwind CSS
+import '@/assets/css/tailwind.css';
+
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// custom css
+import '@/assets/scss/_root.scss';
+import '@/assets/scss/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
